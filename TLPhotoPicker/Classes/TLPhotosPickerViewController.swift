8cf0bae508360275b7400c8ab2ce7db7057baccf
Shah Newaz Hossain <shah.newaz@fieldnation.com>
2023-11-06 23:50:14 +0600

    camera permission

--- TLPhotoPicker/Classes/TLPhotosPickerViewController.swift
+++ TLPhotoPicker/Classes/TLPhotosPickerViewController.swift
@@ -693,7 +693,11 @@ extension TLPhotosPickerViewController: UIImagePickerControllerDelegate, UINavig
                 return
         }
 
-        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        if #available(iOS 10.0, *) {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        } else {
+            // Fallback on earlier versions
+        }
     }
     
     private func handleDeniedCameraAuthorization() {
